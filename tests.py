e2467f0dd798cbf793d5c328306bebefad758c04
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-06 11:47:09 -0500

    Add files via upload

--- tests.py
+++ tests.py
@@ -5,42 +5,43 @@ Calificación del laboratorio
 
 import sys
 
-import soluciones
-
-# import preguntas
-preguntas = soluciones
+import preguntas
 
 
 def test_01():
-    # ---< Input/Output test case >----------------------------------------------------
-    # Pregunta 01
-    # pip3 install scikit-learn==1.0.2 pandas==1.1.5 numpy
-    # python3 tests.py 01
+    """
+    ---< Input/Output test case >----------------------------------------------------
+    Pregunta 01
+    pip3 install scikit-learn pandas numpy
+    python3 tests.py 01
+    """
 
-    X, y = preguntas.pregunta_01()
+    x_data, y_data = preguntas.pregunta_01()
 
-    assert X.shape == (1000, 20)
-    assert y.shape == (1000,)
-    assert "default" not in X.columns
+    assert x_data.shape == (1000, 20)
+    assert y_data.shape == (1000,)
+    assert "default" not in x_data.columns
 
 
 def test_02():
-    # ---< Input/Output test case >----------------------------------------------------
-    # Pregunta 02
-    # pip3 install scikit-learn==1.0.2 pandas==1.1.5 numpy
-    # python3 tests.py 02
+    """
+    ---< Input/Output test case >----------------------------------------------------
+    Pregunta 02
+    pip3 install scikit-learn pandas numpy
+    python3 tests.py 02
+    """
 
-    X_train, X_test, y_train, y_test = preguntas.pregunta_02()
+    x_train, x_test, y_train, y_test = preguntas.pregunta_02()
 
     assert y_train.value_counts().to_dict() == {1: 634, 2: 266}
     assert y_test.value_counts().to_dict() == {1: 66, 2: 34}
-    assert X_train.iloc[:, 0].value_counts().to_dict() == {
+    assert x_train.iloc[:, 0].value_counts().to_dict() == {
         "unknown": 368,
         "1 - 200 DM": 240,
         "< 0 DM": 233,
         "> 200 DM": 59,
     }
-    assert X_test.iloc[:, 0].value_counts().to_dict() == {
+    assert x_test.iloc[:, 0].value_counts().to_dict() == {
         "< 0 DM": 41,
         "1 - 200 DM": 29,
         "unknown": 26,
@@ -49,23 +50,27 @@ def test_02():
 
 
 def test_03():
-    # ---< Run command >-----------------------------------------------------------------
-    # Pregunta 03
-    # pip3 install scikit-learn==1.0.2 pandas==1.1.5 numpy
-    # python3 tests.py 03
+    """
+    ---< Run command >-----------------------------------------------------------------
+    Pregunta 03
+    pip3 install scikit-learn pandas numpy
+    python3 tests.py 03
+    """
 
-    X_train, X_test, y_train, y_test = preguntas.pregunta_02()
+    x_train, x_test, y_train, y_test = preguntas.pregunta_02()
     pipeline = preguntas.pregunta_03()
 
-    assert pipeline.score(X_train, y_train).round(4) == 0.7133
-    assert pipeline.score(X_test, y_test).round(4) == 0.7000
+    assert pipeline.score(x_train, y_train).round(4) == 0.7133
+    assert pipeline.score(x_test, y_test).round(4) == 0.7000
 
 
 def test_04():
-    # ---< Run command >--------------------------------------------------------------------
-    # Pregunta 04
-    # pip3 install scikit-learn==1.0.2 pandas==1.1.5 numpy
-    # python3 tests.py 04
+    """
+    ---< Run command >--------------------------------------------------------------------
+    Pregunta 04
+    pip3 install scikit-learn pandas numpy
+    python3 tests.py 04
+    """
 
     cfm_train, cfm_test = preguntas.pregunta_04()
 
